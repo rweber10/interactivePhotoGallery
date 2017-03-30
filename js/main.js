037ed1021a142d8b63b556b4b315b3cfe566edb5
Robert Weber <robertweber201686@gmail.com>
2017-03-30 11:22:18 -0400

    search bar fixed

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
-$("#filter").keyup(function(){
-  var $filter = $(".imageGallery img").filter().attr("data-title");
-  if $(("#filter").search() === $filter.toLowerCase()) {
-    $("imageGallery img").show();
-  } else {
-    $("imageGallery img").hide();
-  }
+$(function() {
+  // Event on keyup on search
+  $('#filter').keyup(function() {
+    // save the value
+    var srch = this.value;
+    // get all the <li> elements in the gallery
+    // and iterate over them
+    $('.imageGallery li').each(function() {
+      // get the title by using this to target
+      // the select on <a>
+      var title = $('a', this).data('title');
+      // Check if the srch exists in the title
+      // show and hide accordingly
+      if (title.indexOf(srch) != -1) {
+        $(this).show()
+      }
+      else {
+        $(this).hide();
+      }
+    });
+  });
 });
